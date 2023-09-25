5fae814279d397ee74df0c3d8675ef46d4b0a8cd
Simon Toft Stampe Kjeldsen <134495024+Mingithubprofil@users.noreply.github.com>
2023-09-25 15:42:24 +0200

    Update server.js
    changes

--- server/server.js
+++ server/server.js
@@ -10,9 +10,9 @@ const customerRoute = require("./routes/customer");
 app.use(cors());
 app.use(express.json());
 
-app.use("/customer", customerRoute);
-
 app.use(express.static(path.join(__dirname, 'client')));
+
+app.use("/customer", customerRoute);
 
 app.get("/styles/global.css", (req, res) => {
   const filePath = path.join(__dirname, '../client/styles/global.css'); // Juster stien til CSS-filen
