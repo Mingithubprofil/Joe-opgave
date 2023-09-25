b94a81027395f0740af5959b16ec287cfa481dc7
Simon Toft Stampe Kjeldsen <134495024+Mingithubprofil@users.noreply.github.com>
2023-09-25 14:09:11 +0200

    Update server.js
    mime

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path")
+const mime = require("mime")
 
 const app = express();
 
@@ -15,6 +16,13 @@ app.get("/", (req, res) => {
   const filePath = path.join(__dirname, '../client/pages/home.html');
   res.sendFile(filePath);
 });
+
+app.use('/client/styles', express.static(path.join(__dirname, 'client/styles'), {
+  setHeaders: (res, filePath) => {
+    const contentType = mime.getType(filePath);
+    res.setHeader('Content-Type', contentType);
+  },
+}));
 
 app.listen(3000, () => {
   console.log("Server open on port 3000");
