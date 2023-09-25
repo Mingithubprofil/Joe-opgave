baef8c14220a5d1474e28135a7dad4ba4155041e
Simon Toft Stampe Kjeldsen <134495024+Mingithubprofil@users.noreply.github.com>
2023-09-25 15:05:08 +0200

    Update server.js
    css files

--- server/server.js
+++ server/server.js
@@ -12,12 +12,19 @@ app.use(express.json());
 
 app.use("/customer", customerRoute);
 
+app.get('/styles/global.css', (req, res) => {
+  res.setHeader('Content-Type', 'text/css');
+  res.sendFile(__dirname + '/client/styles/global.css');
+});
+
 app.get("/", (req, res) => {
   const filePath = path.join(__dirname, '../client/pages/home.html');
   const contentType = mime.getType(filePath);
   res.setHeader('Content-Type', contentType);
   res.sendFile(filePath);
 });
+
+
 
 
 app.use(express.static(path.join(__dirname, 'client')));
